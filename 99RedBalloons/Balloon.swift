f9c6ad1e5aff55fd2946e72a51dbdd8895c89911
gnazarkin <gleb.nazarkin@gmail.com>
2014-12-12 13:53:33 -0500

    Completed Solution

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,16 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Gleb Nazarkin on 2014-12-12.
+//  Copyright (c) 2014 Addo Labs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var amount = 0
+    var image = UIImage(named: "")
+    
+}
--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -10,16 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var balloonImage: UIImageView!
+    @IBOutlet weak var balloonLabel: UILabel!
+    
+    var myBalloons:[Balloon] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for var i = 0; i < 99; i++ {
+            var balloon = Balloon()
+            balloon.amount = i
+    
+            let randomNumber = Int(arc4random_uniform(UInt32(5)))
+            
+            if randomNumber == 0 {
+                balloon.image = UIImage(named: "BerlinTVTower.jpg")
+            }
+            else if randomNumber == 1 {
+                balloon.image = UIImage(named: "RedBalloon1.jpg")
+            }
+            else if randomNumber == 2 {
+                balloon.image = UIImage(named: "RedBalloon2.jpg")
+            }
+            else if randomNumber == 3 {
+                balloon.image = UIImage(named: "RedBalloon3.jpg")
+            }
+            else {
+                balloon.image = UIImage(named: "RedBalloon4.jpg")
+            }
+            
+            self.myBalloons.append(balloon)
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    @IBAction func nextPressed(sender: AnyObject) {
+        let randomNumber = Int(arc4random_uniform(UInt32(99)))
+        var balloon = myBalloons[randomNumber]
+        balloonImage.image = balloon.image
+        balloonLabel.text = "\(balloon.amount) Balloons"
+    }
+    
 
 }
 
